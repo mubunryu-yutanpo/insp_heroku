20c0c7c1ced6b051204161ec22f388dcf0fdf976
mubunryu-yutanpo <1212yutaro@gmail.com>
2023-06-13 12:44:36 +0900

    0613

--- app/Http/Requests/ValidRequest.php
+++ app/Http/Requests/ValidRequest.php
@@ -29,10 +29,10 @@ class ValidRequest extends FormRequest
             'email'        => 'sometimes|required|string|max:255',
             'password'     => 'sometimes|required|string|max:255|min:8',
             'introduction' => 'sometimes|nullable|string|max:255',
-            'avatar'       => 'sometimes|nullable|mimes:jpg,jpeg,png,gif|max:1024',
+            'avatar'       => 'sometimes|nullable|mimes:jpg,jpeg,png,gif|max:3072',
             'title'        => 'sometimes|required|string|max:255',
             'category'     => 'sometimes|required|',
-            'sumbnail'     => 'sometimes|nullable|mimes:jpg,jpeg,png,gif|max:1024',
+            'sumbnail'     => 'sometimes|nullable|mimes:jpg,jpeg,png,gif|max:3072',
             'summary'      => 'sometimes|required|string|max:255',
             'description'  => 'sometimes|required|string|max:2000',
             'price'        => 'sometimes|required|integer|min:0|max:999999999|regex:/^[0-9]+$/',
@@ -47,9 +47,9 @@ class ValidRequest extends FormRequest
             //フォームリクエスト（バリデーション）のエラーメッセージ設定
             'category.required' => '選択してください',
             'avatar.mimes' => 'ファイル形式はjpeg(jpg)、png、gifが利用可能です',
-            'avatar.max'   => 'ファイルサイズは1MB以下にしてください',
+            'avatar.max'   => 'ファイルサイズは3MB以下にしてください',
             'sumbnail.mimes' => 'ファイル形式はjpeg(jpg)、png、gifが利用可能です',
-            'sumbnail.max'   => 'ファイルサイズは1MB以下にしてください',
+            'sumbnail.max'   => 'ファイルサイズは3MB以下にしてください',
 
         ];
     }
--- resources/views/layouts/form.blade.php
+++ resources/views/layouts/form.blade.php
@@ -1,10 +1,10 @@
 @csrf
 
   <div class="c-form">
-    <div class="c-form__wrap">
+    <div class="c-form__wrap wrap-category">
         <label for="category" class="c-form__label">カテゴリー:</label>
         <select name="category" id="category" class="c-form__input @error('category') valid-error @enderror">
-        <option value="">選択してください</option>
+        <option hidden>選択してください</option>
         @foreach ($category as $cat)
             <option value="{{ $cat['id'] }}" {{ old('category', $idea->category_id ?? '') == $cat['id'] ? 'selected' : '' }}>{{ $cat['name'] }}</option>
         @endforeach
@@ -22,12 +22,38 @@
   <div class="c-form">
     <div class="c-form__wrap">
         <label for="title" class="c-form__label">アイデア名:</label>
-        <input type="text" name="title" id="title" class="c-form__input @error('title') valid-error @enderror" autocomplete="title" value="{{ old('title', $idea->title ?? '') }}">
+        <input type="text" name="title" id="title" class="c-form__input @error('title') valid-error @enderror" autocomplete="title" value="{{ old('title', $idea->title ?? '') }}" placeholder="50文字以内で入力してください">
     </div>
     @error('title')
     <span class="c-form__error" role="alert">
         <strong>{{ $message }}</strong>
     </span>
+    @enderror
+  </div>
+
+  <div class="c-form">
+    <div class="c-form__wrap">
+      <label for="summary" class="c-form__label">概要:</label>
+      <input type="text" name="summary" id="summary" class="c-form__input @error('summary') valid-error @enderror" autocomplete="summary" value="{{ old('summary', $idea->summary ?? '') }}" placeholder="内容の抜粋など">
+    </div>
+    @error('summary')
+      <span class="c-form__error" role="alert">
+        <strong>{{ $message }}</strong>
+      </span>
+    @enderror
+  </div>
+
+  <div class="c-form">
+    <div class="c-form__wrap">
+      <label for="description" class="c-form__label">内容:</label>
+      <textarea name="description" id="description" cols="30" rows="10" class="c-form__input input-description @error('description') valid-error @enderror" autocomplete="description" maxlength="2000" placeholder="アイデアについて2,000文字以内で入力してください">
+        {{ old('description', $idea->description ?? '') }}
+    </textarea>
+    </div>
+    @error('description')
+      <span class="c-form__error" role="alert">
+        <strong>{{ $message }}</strong>
+      </span>
     @enderror
   </div>
 
@@ -43,33 +69,9 @@
 
 
   <div class="c-form">
-    <div class="c-form__wrap">
-      <label for="summary" class="c-form__label">概要:</label>
-      <input type="text" name="summary" id="summary" class="c-form__input @error('summary') valid-error @enderror" autocomplete="summary" value="{{ old('summary', $idea->summary ?? '') }}">
-    </div>
-    @error('summary')
-      <span class="c-form__error" role="alert">
-        <strong>{{ $message }}</strong>
-      </span>
-    @enderror
-  </div>
-
-  <div class="c-form">
-    <div class="c-form__wrap">
-      <label for="description" class="c-form__label">内容:</label>
-      <input type="text" name="description" id="description" class="c-form__input @error('description') valid-error @enderror" autocomplete="description" value="{{ old('description', $idea->description ?? '') }}">
-    </div>
-    @error('description')
-      <span class="c-form__error" role="alert">
-        <strong>{{ $message }}</strong>
-      </span>
-    @enderror
-  </div>
-
-  <div class="c-form">
-    <div class="c-form__wrap">
+    <div class="c-form__wrap wrap-price">
       <label for="price" class="c-form__label">値段:</label>
-      <input type="text" name="price" id="price" class="c-form__input @error('price') valid-error @enderror" autocomplete="price" value="{{ old('price', $idea->price ?? '') }}">
+      <input type="text" name="price" id="price" class="c-form__input @error('price') valid-error @enderror" autocomplete="price" value="{{ old('price', $idea->price ?? '') }}" placeholder="半角数字のみ">
     </div>
     @error('price')
       <span class="c-form__error" role="alert">
