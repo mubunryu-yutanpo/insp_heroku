1417e82d43e003d836293a6c112679b0e64b76fb
yutaro <yutaro@yutaronoMacBook-Pro.local>
2023-07-27 22:46:47 +0900

    0727

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -76,6 +76,7 @@ return [
         'string' => 'The :attribute must be less than or equal :value characters.',
         'array' => 'The :attribute must not have more than :value items.',
     ],
+    
     'max' => [
         'numeric' => 'The :attribute may not be greater than :max.',
         'file' => ':attribute は、300MB以内のものを選択してください。',
